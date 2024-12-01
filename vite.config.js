647fcc27ec5f56a4ea21c13be6188bbc63687068
Lucifer123Morning <listopadov2744@gmail.com>
2024-12-01 13:06:18 -0800

    Initial commit of project

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["three"], // Убедитесь, что three оптимизируется Vite
+  },
+  build: {
+    rollupOptions: {
+      external: ["three"], // Исключаем three из финальной сборки, чтобы она обрабатывалась корректно
+    },
+    commonjsOptions: {
+      include: [/node_modules/, /three/], // Обрабатываем зависимости three из node_modules
+    },
+  },
 });
